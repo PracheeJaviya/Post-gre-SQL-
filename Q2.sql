96bffd4f0d0a5941502af3ab4e3dd80305a8b586
Prachee Javiya <prachee.j@ahduni.edu.in>
2020-03-16 23:52:48 +0530

    'Added'

---
+++ Q2.sql
@@ -0,0 +1,23 @@
+\c dbms
+
+create or replace function dudney(number integer) returns void as $$
+declare
+ y integer := 0;
+ n integer := number;
+ rem integer;
+ z integer;
+
+begin
+loop
+exit when number=0;
+   rem=mod(number,10);
+   number=Trunc(number/10);
+   y=y+rem;
+
+end loop;
+z:= power(y,3);
+if  z = n then
+ raise notice '% is dudney number', number;
+end if;
+end;
+$$ Language plpgsql;
