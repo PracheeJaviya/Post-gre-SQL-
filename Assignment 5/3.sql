30eae80232843313a7766e0436f17c02ac102b98
Prachee Javiya <prachee.j@ahduni.edu.in>
2020-03-29 12:59:33 +0530

    'easy'

---
+++ Assignment 5/3.sql
@@ -0,0 +1,14 @@
+create or replace function edpt_det(id text)
+returns void as
+$$
+declare
+a text;
+b text;
+c text;
+begin
+select * into a,b,c from department where id=dept_id;
+raise notice 'Department id %',a;
+raise notice 'Department Name %',b;
+raise notice 'Head %',c;
+end
+$$language plpgsql;
