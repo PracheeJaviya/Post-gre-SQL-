523055172becfa25e505c836fa95de52a995e678
Prachee Javiya <prachee.j@ahduni.edu.in>
2020-03-30 22:59:05 +0530

    Update 8.sql

--- Assignment 5/8.sql
+++ Assignment 5/8.sql
@@ -1,38 +1,3 @@
--- create or replace function wise() returns void as
--- $$
--- declare
--- id integer;
--- begin
--- raise 'Position wise details : \n'
--- select count(phy_id) from physician;
--- select position into pos from physician;
--- for i in 1..id loop
-
--- raise ''
--- end loop;
--- end
--- $$language plpgsql;
-
--- create or replace function join1() returns void as
--- $$
--- declare
---  cursor c is SELECT * FROM physician INNER JOIN department ON physician.phy_Name=department.head;
---  r c%rowtype;
-
-
--- begin
--- --   dbms_output.put_line('Phy_id Phy_name Position Dept_name');                                                                                  Phy_name Position Dept_name');
---   for r in c loop
---             raise '%  %  %  %',r.phy_id,r.phy_name,r.position,r.dept_name;
---             --  dbms_output.put_line(r.Phy_id||r.Phy_name||'     '||r.Position||r.Dept_name );
---         end loop;
-
--- end;
--- $$language plpgsql;
--- -- declare
--- -- begin
--- -- join();
--- -- end;
 
 CREATE OR REPLACE FUNCTION q8 ()
     RETURNS TABLE(phy_id text,name text,dept_id text,dept_name varchar[30],pos TEXT)
