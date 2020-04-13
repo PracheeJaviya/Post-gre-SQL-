450dc49396b6772c0dd943dc93a6a47670a31a71
Prachee Javiya <prachee.j@ahduni.edu.in>
2020-04-13 18:31:48 +0530

    nw

--- Assignment 6/5.sql
+++ Assignment 6/5.sql
@@ -1,1 +1,14 @@
 -- Create a trigger which will update Room no. in Appointment table is Room no. is updated in Room table.
+create or replace function q5() returns trigger as
+$$
+declare
+begin
+    if TG_OP='update' then
+        update  appointment set mo=NEW.rno where mo=OLD.rno;
+    end if;
+    return NEW;
+end;
+$$language plpgsql;
+
+create trigger q5 before update on room
+for each row execute function q5();
